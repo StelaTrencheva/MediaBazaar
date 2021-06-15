64ba30409f8d614f22ac3f9c34d7bbdafb8811d0
Valeva, Veronika V <i450701>
2021-06-15 15:29:26 +0200

    Updated the GUI of product statistics, added table in the db for the sold products, updated the DBMediatorProducts class

--- Solution/MediaBazaar/MediaBazaar/UserControls/ProductStatisticUC.Designer.cs
+++ Solution/MediaBazaar/MediaBazaar/UserControls/ProductStatisticUC.Designer.cs
@@ -29,85 +29,193 @@ namespace MediaBazaar
         /// </summary>
         private void InitializeComponent()
         {
-            this.nudAmount = new System.Windows.Forms.NumericUpDown();
-            this.lblAmount = new System.Windows.Forms.Label();
             this.lblStatistics = new System.Windows.Forms.Label();
             this.lbxDisplayPRevenueProfit = new System.Windows.Forms.ListBox();
-            ((System.ComponentModel.ISupportInitialize)(this.nudAmount)).BeginInit();
+            this.gbxProductInformation = new System.Windows.Forms.GroupBox();
+            this.lblProductBrand = new System.Windows.Forms.Label();
+            this.lblProductTypeAndModel = new System.Windows.Forms.Label();
+            this.lblProductDescription = new System.Windows.Forms.Label();
+            this.lblPriceSale = new System.Windows.Forms.Label();
+            this.lblSalePrice = new System.Windows.Forms.Label();
+            this.lblPriceCost = new System.Windows.Forms.Label();
+            this.lblCostPrice = new System.Windows.Forms.Label();
+            this.lblSoldProducts = new System.Windows.Forms.Label();
+            this.pnlAmountInWarehouse = new System.Windows.Forms.Panel();
+            this.lblAmountOfSoldProducts = new System.Windows.Forms.Label();
+            this.gbxProductInformation.SuspendLayout();
+            this.pnlAmountInWarehouse.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // nudAmount
-            // 
-            this.nudAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.nudAmount.Location = new System.Drawing.Point(1151, 108);
-            this.nudAmount.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.nudAmount.Name = "nudAmount";
-            this.nudAmount.ReadOnly = true;
-            this.nudAmount.Size = new System.Drawing.Size(47, 30);
-            this.nudAmount.TabIndex = 11;
-            this.nudAmount.Value = new decimal(new int[] {
-            5,
-            0,
-            0,
-            0});
-            this.nudAmount.ValueChanged += new System.EventHandler(this.nudAmount_ValueChanged);
-            // 
-            // lblAmount
-            // 
-            this.lblAmount.AutoSize = true;
-            this.lblAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblAmount.Location = new System.Drawing.Point(916, 112);
-            this.lblAmount.Name = "lblAmount";
-            this.lblAmount.Size = new System.Drawing.Size(228, 25);
-            this.lblAmount.TabIndex = 10;
-            this.lblAmount.Text = "Amount of sold products:";
             // 
             // lblStatistics
             // 
-            this.lblStatistics.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblStatistics.Location = new System.Drawing.Point(7, 36);
+            this.lblStatistics.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblStatistics.Location = new System.Drawing.Point(26, 28);
             this.lblStatistics.Name = "lblStatistics";
-            this.lblStatistics.Size = new System.Drawing.Size(1251, 50);
+            this.lblStatistics.Size = new System.Drawing.Size(255, 40);
             this.lblStatistics.TabIndex = 9;
-            this.lblStatistics.Text = "All products";
-            this.lblStatistics.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblStatistics.Text = "Product Statistics";
             // 
             // lbxDisplayPRevenueProfit
             // 
-            this.lbxDisplayPRevenueProfit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbxDisplayPRevenueProfit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.lbxDisplayPRevenueProfit.FormattingEnabled = true;
             this.lbxDisplayPRevenueProfit.HorizontalExtent = 1000;
             this.lbxDisplayPRevenueProfit.HorizontalScrollbar = true;
             this.lbxDisplayPRevenueProfit.ItemHeight = 25;
-            this.lbxDisplayPRevenueProfit.Location = new System.Drawing.Point(3, 141);
-            this.lbxDisplayPRevenueProfit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.lbxDisplayPRevenueProfit.Location = new System.Drawing.Point(32, 82);
             this.lbxDisplayPRevenueProfit.Name = "lbxDisplayPRevenueProfit";
             this.lbxDisplayPRevenueProfit.ScrollAlwaysVisible = true;
-            this.lbxDisplayPRevenueProfit.Size = new System.Drawing.Size(1230, 754);
+            this.lbxDisplayPRevenueProfit.Size = new System.Drawing.Size(509, 479);
             this.lbxDisplayPRevenueProfit.TabIndex = 8;
+            this.lbxDisplayPRevenueProfit.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.lbxDisplayPRevenueProfit_DrawItem);
+            this.lbxDisplayPRevenueProfit.DoubleClick += new System.EventHandler(this.lbxDisplayPRevenueProfit_DoubleClick);
+            this.lbxDisplayPRevenueProfit.MouseLeave += new System.EventHandler(this.lbxDisplayPRevenueProfit_MouseLeave);
+            this.lbxDisplayPRevenueProfit.MouseMove += new System.Windows.Forms.MouseEventHandler(this.lbxDisplayPRevenueProfit_MouseMove);
+            // 
+            // gbxProductInformation
+            // 
+            this.gbxProductInformation.BackColor = System.Drawing.Color.White;
+            this.gbxProductInformation.Controls.Add(this.pnlAmountInWarehouse);
+            this.gbxProductInformation.Controls.Add(this.lblSoldProducts);
+            this.gbxProductInformation.Controls.Add(this.lblCostPrice);
+            this.gbxProductInformation.Controls.Add(this.lblPriceCost);
+            this.gbxProductInformation.Controls.Add(this.lblSalePrice);
+            this.gbxProductInformation.Controls.Add(this.lblPriceSale);
+            this.gbxProductInformation.Controls.Add(this.lblProductDescription);
+            this.gbxProductInformation.Controls.Add(this.lblProductBrand);
+            this.gbxProductInformation.Controls.Add(this.lblProductTypeAndModel);
+            this.gbxProductInformation.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.gbxProductInformation.Location = new System.Drawing.Point(569, 82);
+            this.gbxProductInformation.Name = "gbxProductInformation";
+            this.gbxProductInformation.Size = new System.Drawing.Size(517, 479);
+            this.gbxProductInformation.TabIndex = 10;
+            this.gbxProductInformation.TabStop = false;
+            // 
+            // lblProductBrand
+            // 
+            this.lblProductBrand.AutoSize = true;
+            this.lblProductBrand.Location = new System.Drawing.Point(30, 84);
+            this.lblProductBrand.Name = "lblProductBrand";
+            this.lblProductBrand.Size = new System.Drawing.Size(83, 29);
+            this.lblProductBrand.TabIndex = 1;
+            this.lblProductBrand.Text = "Brand ";
+            // 
+            // lblProductTypeAndModel
+            // 
+            this.lblProductTypeAndModel.AutoSize = true;
+            this.lblProductTypeAndModel.Location = new System.Drawing.Point(30, 31);
+            this.lblProductTypeAndModel.Name = "lblProductTypeAndModel";
+            this.lblProductTypeAndModel.Size = new System.Drawing.Size(188, 29);
+            this.lblProductTypeAndModel.TabIndex = 0;
+            this.lblProductTypeAndModel.Text = "Type and model";
+            // 
+            // lblProductDescription
+            // 
+            this.lblProductDescription.AutoSize = true;
+            this.lblProductDescription.Location = new System.Drawing.Point(30, 138);
+            this.lblProductDescription.Name = "lblProductDescription";
+            this.lblProductDescription.Size = new System.Drawing.Size(135, 29);
+            this.lblProductDescription.TabIndex = 2;
+            this.lblProductDescription.Text = "Description";
+            // 
+            // lblPriceSale
+            // 
+            this.lblPriceSale.AutoSize = true;
+            this.lblPriceSale.Location = new System.Drawing.Point(30, 192);
+            this.lblPriceSale.Name = "lblPriceSale";
+            this.lblPriceSale.Size = new System.Drawing.Size(134, 29);
+            this.lblPriceSale.TabIndex = 3;
+            this.lblPriceSale.Text = "Sale price: ";
+            // 
+            // lblSalePrice
+            // 
+            this.lblSalePrice.AutoSize = true;
+            this.lblSalePrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblSalePrice.ForeColor = System.Drawing.Color.Maroon;
+            this.lblSalePrice.Location = new System.Drawing.Point(235, 189);
+            this.lblSalePrice.Name = "lblSalePrice";
+            this.lblSalePrice.Size = new System.Drawing.Size(49, 32);
+            this.lblSalePrice.TabIndex = 4;
+            this.lblSalePrice.Text = "00";
+            // 
+            // lblPriceCost
+            // 
+            this.lblPriceCost.AutoSize = true;
+            this.lblPriceCost.Location = new System.Drawing.Point(30, 256);
+            this.lblPriceCost.Name = "lblPriceCost";
+            this.lblPriceCost.Size = new System.Drawing.Size(128, 29);
+            this.lblPriceCost.TabIndex = 5;
+            this.lblPriceCost.Text = "Cost price:";
+            // 
+            // lblCostPrice
+            // 
+            this.lblCostPrice.AutoSize = true;
+            this.lblCostPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblCostPrice.ForeColor = System.Drawing.Color.Maroon;
+            this.lblCostPrice.Location = new System.Drawing.Point(235, 256);
+            this.lblCostPrice.Name = "lblCostPrice";
+            this.lblCostPrice.Size = new System.Drawing.Size(49, 32);
+            this.lblCostPrice.TabIndex = 6;
+            this.lblCostPrice.Text = "00";
+            // 
+            // lblSoldProducts
+            // 
+            this.lblSoldProducts.AutoSize = true;
+            this.lblSoldProducts.Location = new System.Drawing.Point(30, 323);
+            this.lblSoldProducts.Name = "lblSoldProducts";
+            this.lblSoldProducts.Size = new System.Drawing.Size(253, 29);
+            this.lblSoldProducts.TabIndex = 7;
+            this.lblSoldProducts.Text = "Sold products in store:";
+            // 
+            // pnlAmountInWarehouse
+            // 
+            this.pnlAmountInWarehouse.BackColor = System.Drawing.Color.YellowGreen;
+            this.pnlAmountInWarehouse.Controls.Add(this.lblAmountOfSoldProducts);
+            this.pnlAmountInWarehouse.Location = new System.Drawing.Point(241, 377);
+            this.pnlAmountInWarehouse.Name = "pnlAmountInWarehouse";
+            this.pnlAmountInWarehouse.Size = new System.Drawing.Size(178, 63);
+            this.pnlAmountInWarehouse.TabIndex = 8;
+            // 
+            // lblAmountOfSoldProducts
+            // 
+            this.lblAmountOfSoldProducts.AutoSize = true;
+            this.lblAmountOfSoldProducts.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblAmountOfSoldProducts.Location = new System.Drawing.Point(71, 16);
+            this.lblAmountOfSoldProducts.Name = "lblAmountOfSoldProducts";
+            this.lblAmountOfSoldProducts.Size = new System.Drawing.Size(39, 29);
+            this.lblAmountOfSoldProducts.TabIndex = 5;
+            this.lblAmountOfSoldProducts.Text = "00";
             // 
             // ProductStatisticInterface
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.nudAmount);
-            this.Controls.Add(this.lblAmount);
+            this.Controls.Add(this.gbxProductInformation);
             this.Controls.Add(this.lblStatistics);
             this.Controls.Add(this.lbxDisplayPRevenueProfit);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "ProductStatisticInterface";
-            this.Size = new System.Drawing.Size(1261, 934);
-            ((System.ComponentModel.ISupportInitialize)(this.nudAmount)).EndInit();
+            this.Size = new System.Drawing.Size(1121, 747);
+            this.gbxProductInformation.ResumeLayout(false);
+            this.gbxProductInformation.PerformLayout();
+            this.pnlAmountInWarehouse.ResumeLayout(false);
+            this.pnlAmountInWarehouse.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.NumericUpDown nudAmount;
-        private System.Windows.Forms.Label lblAmount;
         private System.Windows.Forms.Label lblStatistics;
         private System.Windows.Forms.ListBox lbxDisplayPRevenueProfit;
+        private System.Windows.Forms.GroupBox gbxProductInformation;
+        private System.Windows.Forms.Label lblProductBrand;
+        private System.Windows.Forms.Label lblProductTypeAndModel;
+        private System.Windows.Forms.Label lblProductDescription;
+        private System.Windows.Forms.Label lblSalePrice;
+        private System.Windows.Forms.Label lblPriceSale;
+        private System.Windows.Forms.Label lblCostPrice;
+        private System.Windows.Forms.Label lblPriceCost;
+        private System.Windows.Forms.Label lblSoldProducts;
+        private System.Windows.Forms.Panel pnlAmountInWarehouse;
+        private System.Windows.Forms.Label lblAmountOfSoldProducts;
     }
 }
--- Solution/MediaBazaar/MediaBazaar/UserControls/ProductStatisticUC.cs
+++ Solution/MediaBazaar/MediaBazaar/UserControls/ProductStatisticUC.cs
@@ -14,25 +14,85 @@ namespace MediaBazaar
     public partial class ProductStatisticInterface : UserControl
     {
         ProductManager pManager;
+        private int prevIndex = -1;
 
         public ProductStatisticInterface()
         {
             InitializeComponent();
             this.pManager = new ProductManager();
             UpdateListBoxAllProducts();
+            lbxDisplayPRevenueProfit.DrawMode = DrawMode.OwnerDrawFixed;
+            lbxDisplayPRevenueProfit.DrawItem += new DrawItemEventHandler(lbxDisplayPRevenueProfit_DrawItem);
         }
 
-        private void nudAmount_ValueChanged(object sender, EventArgs e)
-        {
-            UpdateListBoxAllProducts();
-        }
         public void UpdateListBoxAllProducts()
         {
             lbxDisplayPRevenueProfit.Items.Clear();
-            foreach (Product i in this.pManager.GetAllProducts())
+            foreach (Product product in this.pManager.GetAllProducts())
             {
-                lbxDisplayPRevenueProfit.Items.Add(i.GetStatistic(Convert.ToInt32(nudAmount.Value)));
+                lbxDisplayPRevenueProfit.Items.Add($"{product.Type} - {product.Model} ({product.Brand})");
             }
+        }
+        private void lbxDisplayPRevenueProfit_DoubleClick(object sender, EventArgs e)
+        {
+            foreach (Product product in this.pManager.GetAllProducts())
+            {
+                if (lbxDisplayPRevenueProfit.Items[prevIndex].ToString() == $"{product.Type} - {product.Model} ({product.Brand})")
+                {
+                    lblProductTypeAndModel.Text = $"{product.Type} - {product.Model}";
+                    lblProductBrand.Text = $"{product.Brand}";
+                    lblProductDescription.Text = $"{product.Description}";
+                    lblSalePrice.Text = $"{product.SalePrice}";
+                    lblCostPrice.Text = $"{product.CostPrice}";
+                    lblAmountOfSoldProducts.Text = "00";
+                    foreach (var soldProduct in pManager.GetListOfSoldProducts())
+                    {
+                        if (soldProduct.Key.PNumber == product.PNumber)
+                        {
+                            lblAmountOfSoldProducts.Text = soldProduct.Value.ToString();
+                        }
+                    }
+                }
+            }
+        }
+        private void lbxDisplayPRevenueProfit_MouseMove(object sender, MouseEventArgs e)
+        {
+            int index = lbxDisplayPRevenueProfit.IndexFromPoint(lbxDisplayPRevenueProfit.PointToClient(Cursor.Position));
+            if (index != prevIndex)
+            {
+                prevIndex = index;
+                lbxDisplayPRevenueProfit.Invalidate();
+            }
+        }
+        private void lbxDisplayPRevenueProfit_MouseLeave(object sender, EventArgs e)
+        {
+            prevIndex = -1;
+            lbxDisplayPRevenueProfit.Invalidate();
+        }
+
+        private void lbxDisplayPRevenueProfit_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            e.DrawBackground();
+            Graphics g = e.Graphics;
+            Color c;
+            if (e.Index == prevIndex)
+            {
+                c = Color.Yellow;
+            }
+            else
+            {
+                c = Color.LightBlue;
+            }
+            using (SolidBrush brsh = new SolidBrush(c))
+            {
+                g.FillRectangle(brsh, e.Bounds);
+            }
+            using (SolidBrush brsh = new SolidBrush(e.ForeColor))
+            {
+                g.DrawString(lbxDisplayPRevenueProfit.Items[e.Index].ToString(), e.Font,
+                 brsh, e.Bounds, StringFormat.GenericDefault);
+            }
+            e.DrawFocusRectangle();
         }
 
 
